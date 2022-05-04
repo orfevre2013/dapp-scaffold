8406490eaa0b8d9c40204d9e24bbf6cd8cb4b53b
Houssou Sedjro <toffa.info@gmail.com>
2022-05-04 20:57:45 +0200

    update for cors again

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/solana-api.projectserum.com",
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
