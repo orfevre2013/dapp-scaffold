16a2f1de41b638f6ee0b770cc1ee849d1f444e42
Houssou Sedjro <toffa.info@gmail.com>
2022-05-04 17:03:09 +0200

    update

--- src/views/swap/index.tsx
+++ src/views/swap/index.tsx
@@ -47,7 +47,7 @@ export const SwapView: FC = ({ }) => {
 
 
   const provider = new AnchorProvider(connection, wallet, opts)
-  console.log(tokenList)
+  
   return (
 <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
